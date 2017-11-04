31f3414ca7cc7904ae979a90246ac78538e70faa
JoeMelodicka <regio17@teletu.it>
2017-11-04 22:37:09 +0100

    a

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ function onButtonClick() {
   
   console.log('Requesting Bluetooth Device...');
 
-  navigator.bluetooth.requestDevice(/*{acceptAllDevices: true}*/ { filters: [{ services: [0xfee8, 0xfee9] }] } )
+  navigator.bluetooth.requestDevice(/*{acceptAllDevices: true}*/ { filters: [{ services: [0xfee8, 0xfee9] }] } ) // Services of the Smooth Q
   .then(device => {
     console.log('> Name:             ' + device.name);
     console.log('> Id:               ' + device.id);
-    console.log('> Connected:        ' + device.gatt.connected);
+    //console.log('> Connected:        ' + device.gatt.connected);
+    return device.gatt.connect();
+  })
+  .then(server => {
+    // Getting Battery Service...
+    return server.getPrimaryService(0xfee8);
   })
   .catch(error => {
     console.log('Argh! ' + error);
