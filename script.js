0c8a09dab5eedc67c313de8642377e349ca9db5d
JoeMelodicka <regio17@teletu.it>
2017-11-04 22:26:03 +0100

    a

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
 function onButtonClick() {
-  let filters = [];
-
-  let filterService = document.querySelector('#service').value;
-  if (filterService.startsWith('0x')) {
-    filterService = parseInt(filterService);
-  }
-  if (filterService) {
-    filters.push({services: [filterService]});
-  }
-
-  let filterName = document.querySelector('#name').value;
-  if (filterName) {
-    filters.push({name: filterName});
-  }
-
-  let filterNamePrefix = document.querySelector('#namePrefix').value;
-  if (filterNamePrefix) {
-    filters.push({namePrefix: filterNamePrefix});
-  }
-
-  let options = {};
-  if (document.querySelector('#allDevices').checked) {
-    options.acceptAllDevices = true;
-  } else {
-    options.filters = filters;
-  }
-
+  
   console.log('Requesting Bluetooth Device...');
-  console.log('with ' + JSON.stringify(options));
-  navigator.bluetooth.requestDevice(/*options*/ {acceptAllDevices: true})
+
+  navigator.bluetooth.requestDevice(/*{acceptAllDevices: true}*/ { filters: [{ services: [0xfee8, 0xfee9] }] })
   .then(device => {
     console.log('> Name:             ' + device.name);
     console.log('> Id:               ' + device.id);
